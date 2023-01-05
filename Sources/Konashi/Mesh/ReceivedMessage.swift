6db20d0f8c4f1b97bdfeb2b63f3b39e7f55852db
Akira <akira.matsuda@me.com>
2023-01-05 13:56:44 +0900

    ReceivedMessageの初期化メソッドを追加

--- Sources/Konashi/Mesh/ReceivedMessage.swift
+++ Sources/Konashi/Mesh/ReceivedMessage.swift
@@ -11,4 +11,10 @@ public struct ReceivedMessage {
     public let body: MeshMessage
     public let source: Address
     public let destination: Address
+    
+    public init(body: MeshMessage, source: Address, destination: Address) {
+        self.body = body
+        self.source = source
+        self.destination = destination
+    }
 }
