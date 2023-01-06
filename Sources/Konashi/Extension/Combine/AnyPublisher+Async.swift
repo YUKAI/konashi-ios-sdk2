19978b438b584a70b03b00c1c4b75b3d5116f5bd
Akira <akira.matsuda@me.com>
2023-01-07 02:23:52 +0900

    continuationがresumeされない場合がある問題を修正

--- Sources/Konashi/Extension/Combine/AnyPublisher+Async.swift
+++ Sources/Konashi/Extension/Combine/AnyPublisher+Async.swift
@@ -6,23 +6,40 @@
 //
 
 import Combine
+import Foundation
 
 // https://medium.com/geekculture/from-combine-to-async-await-c08bf1d15b77
+enum AsyncError: Error {
+    case finishedWithoutValue
+}
+
+fileprivate class AnyPublisherCancellable {
+    static let shared = AnyPublisherCancellable()
+    var cancelablle = [UUID: AnyCancellable]()
+}
+
 extension AnyPublisher {
     func async() async throws -> Output {
         try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-            cancellable = first()
+            let uuid = UUID()
+            var finishedWithoutValue = true
+            AnyPublisherCancellable.shared.cancelablle[uuid] = first()
                 .sink { result in
                     switch result {
                     case .finished:
-                        break
+                        if finishedWithoutValue {
+                            continuation.resume(throwing: AsyncError.finishedWithoutValue)
+                        }
                     case let .failure(error):
                         continuation.resume(throwing: error)
                     }
-                    cancellable?.cancel()
+                    AnyPublisherCancellable.shared.cancelablle[uuid]?.cancel()
+                    AnyPublisherCancellable.shared.cancelablle.removeValue(forKey: uuid)
                 } receiveValue: { value in
+                    finishedWithoutValue = false
                     continuation.resume(with: .success(value))
+                    AnyPublisherCancellable.shared.cancelablle[uuid]?.cancel()
+                    AnyPublisherCancellable.shared.cancelablle.removeValue(forKey: uuid)
                 }
         }
     }
