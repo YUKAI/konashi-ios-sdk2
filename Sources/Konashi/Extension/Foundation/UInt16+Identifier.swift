f3c8ae32777fd7d7d70ddfbb5f8a2201d7386d4d
Akira <akira.matsuda@me.com>
2023-01-08 22:32:08 +0900

    IDの定数を追加

---
+++ Sources/Konashi/Extension/Foundation/UInt16+Identifier.swift
@@ -0,0 +1,13 @@
+//
+//  UInt16+Identifier.swift
+//  Konashi
+//
+//  Created by Akira Matsuda on 2023/01/08.
+//
+
+import Foundation
+
+extension UInt16 {
+    static let yukaiCompanyId: UInt16 = 0x0925
+    static let primaryModelId: UInt16 = 0x0001
+}
