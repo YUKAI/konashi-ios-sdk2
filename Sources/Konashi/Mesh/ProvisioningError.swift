4eab26e7936e4eb13a12c728c461f9cb1fbe2616
Akira <akira.matsuda@me.com>
2023-01-10 17:05:12 +0900

    エラーの階層を変更

--- Sources/Konashi/Mesh/ProvisioningError.swift
+++ Sources/Konashi/Mesh/ProvisioningError.swift
@@ -7,24 +7,22 @@
 
 import Foundation
 
-extension MeshProvisioner {
-    public enum ProvisioningError: Error, LocalizedError {
-        case unknown
-        case invalidUnicastAddress
-        case invalidCapability
-        case unsupportedDevice
+public enum ProvisioningError: Error, LocalizedError {
+    case unknown
+    case invalidUnicastAddress
+    case invalidCapability
+    case unsupportedDevice
 
-        public var errorDescription: String? {
-            switch self {
-            case .unknown:
-                return "Unknown error."
-            case .invalidUnicastAddress:
-                return "The device has invalid unicast address."
-            case .invalidCapability:
-                return "Provisioning capability should not be nil."
-            case .unsupportedDevice:
-                return "The device is not able to provision."
-            }
+    public var errorDescription: String? {
+        switch self {
+        case .unknown:
+            return "Unknown error."
+        case .invalidUnicastAddress:
+            return "The device has invalid unicast address."
+        case .invalidCapability:
+            return "Provisioning capability should not be nil."
+        case .unsupportedDevice:
+            return "The device is not able to provision."
         }
     }
 }
