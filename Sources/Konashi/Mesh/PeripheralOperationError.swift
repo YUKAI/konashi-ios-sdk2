901fe189f1bfb25a962234e0cb6e457af92fa716
Akira <akira.matsuda@me.com>
2023-01-10 17:02:49 +0900

    エラー名を変更

---
+++ Sources/Konashi/Mesh/PeripheralOperationError.swift
@@ -0,0 +1,26 @@
+//
+//  OperationError.swift
+//  konashi-ios-sdk2
+//
+//  Created by Akira Matsuda on 2023/01/10.
+//
+
+import Foundation
+
+/// An error that a peripheral returns during configure / read / write operation.
+public enum PeripheralOperationError: LocalizedError {
+    case invalidReadValue
+    case couldNotReadValue
+    case couldNotFindCharacteristic
+    
+    public var errorDescription: String? {
+        switch self {
+        case .invalidReadValue:
+            return "Invalid read value."
+        case .couldNotReadValue:
+            return "Could not read value."
+        case .couldNotFindCharacteristic:
+            return "Could not find characteristics."
+        }
+    }
+}
