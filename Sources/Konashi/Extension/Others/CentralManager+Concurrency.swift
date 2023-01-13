e7b707362d3c6b64e756dbbae1173bfed86eadfc
Akira <akira.matsuda@me.com>
2023-01-13 20:35:04 +0900

    CentralManagerにSwift Concurrencyのメソッドを追加

---
+++ Sources/Konashi/Extension/Others/CentralManager+Concurrency.swift
@@ -0,0 +1,43 @@
+//
+//  CentralManager+Concurrency.swift
+//  konashi-ios-sdk2
+//
+//  Created by Akira Matsuda on 2023/01/13.
+//
+
+import CoreBluetooth
+
+public extension CentralManager {
+    /// Attempt to scan available peripherals.
+    func scan(for target: ScanTarget = .all) async {
+        return await withCheckedContinuation { continuation in
+            scan(for: target).then {
+                continuation.resume(returning: ())
+            }
+        }
+    }
+
+    /// Attempt to find a peripheral.
+    /// - Parameters:
+    ///   - name: Peripheral name to find.
+    ///   - timeoutInterval: The duration of timeout.
+    /// - Returns: A peripheral that is found.
+    func find(name: String, timeoutInterval: TimeInterval = 5) async throws -> any Peripheral {
+        return try await withCheckedThrowingContinuation { continuation in
+            find(name: name, timeoutInterval: timeoutInterval).then { peripheral in
+                continuation.resume(returning: peripheral)
+            }.catch { error in
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+
+    /// Stop scanning peripherals.
+    func stopScan() async {
+        return await withCheckedContinuation { continuation in
+            stopScan().then {
+                continuation.resume(returning: ())
+            }
+        }
+    }
+}
