2d0d274fd8ae6f3facaeec20b52efe7974560a61
Akira <akira.matsuda@me.com>
2023-08-20 18:29:08 +0900

    ログを出力する

--- Example/GPIOExample/GPIOExample/AppDelegate.swift
+++ Example/GPIOExample/GPIOExample/AppDelegate.swift
@@ -5,15 +5,28 @@
 //  Created by Akira Matsuda on 2023/08/20.
 //
 
+import Combine
+import CombineExt
+import Konashi
+import os.log
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var cancellable = Set<AnyCancellable>()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        [
+            MeshManager.sharedLogOutput,
+            CentralManager.sharedLogOutput,
+            KonashiPeripheral.sharedLogOutput,
+            MeshNode.sharedLogOutput
+        ].merge()
+            .sink { log in
+                os_log("%{public}@", log: log.osLog, type: log.osLogType, log.description)
+            }.store(in: &cancellable)
+
         return true
     }
 
@@ -30,7 +43,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
